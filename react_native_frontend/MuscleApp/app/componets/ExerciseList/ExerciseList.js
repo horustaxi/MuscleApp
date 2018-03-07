f7ddc317f57c5d6db4ff4d822d4617e0c36c3566
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2018-03-07 07:16:37 -0300

    Exercise List created

---
+++ react_native_frontend/MuscleApp/app/componets/ExerciseList/ExerciseList.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React, { Component } from 'react'
+import { StyleSheet, Image, View, TouchableHighlight, FlatList, Text } from 'react-native';
+
+class ExerciseList extends React.PureComponent {
+    static navigationOptions = {
+        title: 'Exercises',
+    };
+    render() {
+        return(
+            <Text>Testando</Text>
+        ); 
+    }
+}
+
+export default ExerciseList;
