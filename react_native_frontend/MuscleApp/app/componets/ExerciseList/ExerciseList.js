151e134513d6027c0d2c4d64e71b75372327b1f9
Vinicius A dos Santos <vinicius.vas.ti@gmail.com>
2018-03-28 15:00:22 -0300

    http get of exercises on exerciselist

--- react_native_frontend/MuscleApp/app/componets/ExerciseList/ExerciseList.js
+++ react_native_frontend/MuscleApp/app/componets/ExerciseList/ExerciseList.js
@@ -2,12 +2,31 @@
 
 import React from 'react';
 import { Image, View, TouchableHighlight, FlatList, Text } from 'react-native';
+import axios from 'axios';
 import styles from './styles';
+import { Constants } from '../../config/constants';
 
 class ExerciseList extends React.PureComponent {
+
     static navigationOptions = {
         title: 'Exercises',
     };
+
+    constructor(props) {
+        super(props);
+        state = { exercises: [] };
+    }
+
+    componentWillMount() {
+        axios.get(`${Constants.apiUrl}exercise`,
+        {
+            headers: {
+                Authorization: 'Bearer ' + ''
+            }
+        })
+        .then(response => console.log(response));
+    }
+
     render() {
         return (
             <View style={styles.container}>
