dab014cc86fd345ae1bbe0a4560412df15c78d98
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2019-05-08 23:42:12 -0300

    Retornando exercises ativos

--- java_back/MuscleApp/src/main/java/com/vas/muscleapp/repositories/ExerciseRepository.java
+++ java_back/MuscleApp/src/main/java/com/vas/muscleapp/repositories/ExerciseRepository.java
@@ -1,6 +1,7 @@
 package com.vas.muscleapp.repositories;
 
 import com.vas.muscleapp.models.Exercise;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,5 +14,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
     public Optional<Exercise> findExerciseByName(String name);
-    
+    public List<Exercise> findExerciseByActive(Boolean active);
+
 }
