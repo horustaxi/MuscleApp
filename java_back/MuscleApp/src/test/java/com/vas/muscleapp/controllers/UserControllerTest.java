1f9aa32ec24cbf2bb8bd7141a6332067875e4b63
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2019-05-08 21:33:36 -0300

    Teste de requisição de registro de usuário

--- java_back/MuscleApp/src/test/java/com/vas/muscleapp/controllers/UserControllerTest.java
+++ java_back/MuscleApp/src/test/java/com/vas/muscleapp/controllers/UserControllerTest.java
@@ -7,10 +7,7 @@ package com.vas.muscleapp.controllers;
 
 import com.vas.muscleapp.abstracts.BaseControllerTest;
 import com.vas.muscleapp.models.User;
-import com.vas.muscleapp.services.UserService;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,19 +16,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author vinicius
  */
 public class UserControllerTest extends BaseControllerTest {
-    
-    @Autowired
-    private UserService userService;
-    
-    @Before
-    public void setUp() {
-        userService.deleteAll();
-    }
-    
+
     @Test
-    public void testRegister() throws Exception {
+    public void User_RegisterShouldReturnHttpCreated() throws Exception {
         User user = new User("Jack", "jack@email.com", "000000");
-        mockMvc.perform(post("/register").contentType(contentType).content(json(user)))
+        mockMvc.perform(
+                post("/register").contentType(contentType).content(json(user)))
                 .andExpect(status().isCreated());
     }
 
