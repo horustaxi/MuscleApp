97506f2b5df847c6c4d75ccbe215ef48302fcb90
Vinicius A. dos Santos <vinicius.vas.ti@gmail.com>
2019-05-11 13:50:55 -0300

    Improvement

--- java_back/MuscleApp/src/main/java/com/vas/muscleapp/services/ExerciseService.java
+++ java_back/MuscleApp/src/main/java/com/vas/muscleapp/services/ExerciseService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.vas.muscleapp.services;
 
 import java.util.List;
@@ -19,7 +14,7 @@ import com.vas.muscleapp.repositories.ExerciseRepository;
  * @author Vinícius
  */
 @Service
-public class ExerciseService {
+public class ExerciseService implements BaseService<Exercise, Long> {
 
 	private final ExerciseRepository exerciseRepository;
 
